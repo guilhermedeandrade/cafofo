962d5e3751be24ec07749771e7d84522569be58f
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2021-01-10 09:23:31 +0000

    feat: get rent costs from the first page

--- lib/cafofo.ex
+++ lib/cafofo.ex
@@ -1,18 +1,43 @@
 defmodule Cafofo do
-  @moduledoc """
-  Documentation for `Cafofo`.
-  """
+  def fetch_page(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
 
-  @doc """
-  Hello world.
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts("Not found")
 
-  ## Examples
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect(reason)
+    end
+  end
 
-      iex> Cafofo.hello()
-      :world
+  def parse_html(html) do
+    case Floki.parse_document(html) do
+      {:ok, document} ->
+        document
 
-  """
-  def hello do
-    :world
+      {:error, error} ->
+        IO.inspect(error)
+    end
+  end
+
+  def find_items_info(document) do
+    document
+    |> Floki.find("ul#ad-list")
+    |> Floki.find("a > div > div:nth-child(2) > div:first-child")
+  end
+
+  # TODO: figure how to filter out the expensive items
+  def get_rent_cost(document) do
+    Floki.find(document, "div > div:nth-child(2) > div:nth-child(2) span")
+  end
+
+  def get_houses do
+    System.get_env("OLX_URL")
+    |> fetch_page
+    |> parse_html
+    |> find_items_info
+    |> get_rent_cost
   end
 end
