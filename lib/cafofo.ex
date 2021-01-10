6daa2eb33251b17c7de27cd6b9799cdb4605089b
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2021-01-10 07:14:17 +0000

    Initial commit

---
+++ lib/cafofo.ex
@@ -0,0 +1,18 @@
+defmodule Cafofo do
+  @moduledoc """
+  Documentation for `Cafofo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cafofo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cafofo_test.exs
@@ -0,0 +1,8 @@
+defmodule CafofoTest do
+  use ExUnit.Case
+  doctest Cafofo
+
+  test "greets the world" do
+    assert Cafofo.hello() == :world
+  end
+end
